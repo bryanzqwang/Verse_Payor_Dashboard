ad28803eb14e3cf1a1d1e90bbf4b0df30da96048
bryanzqwang <bryan.wang9547@gmail.com>
2026-05-08 14:26:32 -0400

    updated gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "973a0b9770df3fc6"; }
+/******/ 		__webpack_require__.h = function() { return "b8029e5563fe743c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
