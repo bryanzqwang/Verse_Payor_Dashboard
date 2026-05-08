51bca23891197de19c7c7ffcb21a11e16d356fcd
bryanzqwang <bryan.wang9547@gmail.com>
2026-05-08 15:34:46 -0400

    updated delivery time of mail order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -342,7 +342,7 @@ export default function Home(): JSX.Element {
             <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
               <ServiceCategoryCard category="Bent Metal" slaRate="91%" avgDelivery="18h" />
               <ServiceCategoryCard category="Discharge" slaRate="87%" avgDelivery="6h" />
-              <ServiceCategoryCard category="Mail Order" slaRate="94%" avgDelivery="48h" />
+              <ServiceCategoryCard category="Mail Order" slaRate="94%" avgDelivery="37h" />
               <ServiceCategoryCard category="Oxygen" slaRate="89%" avgDelivery="24h" />
             </div>
             <div className="h-[400px] md:h-[600px]">
